ba2c0f613fdbb3206ffd73cc8e53502db17f868d
João Victor da Silva <joaovictor00788123@gmail.com>
2021-01-13 13:40:05 -0300

    Adicionando componente itemSolution

---
+++ src/app/components/itemSolution.js
@@ -0,0 +1,32 @@
+import Ract from 'react'
+import '../css/itemSolution.css'
+
+const ItemSolution = (props) => {
+    return props.type === 1 ? (
+
+        <div className="row" style={{ paddingTop: '50px' }}>
+            <div className="col-3">
+                <div className="circulo1">
+                    <img id="svg" className="svg" src={props.icon} />
+                </div>
+            </div>
+            <div className="col-9">
+                <h4 className="title">{props.title}</h4>
+                <h6 className="subtitle">{props.subtitle}</h6>
+            </div>
+        </div>
+
+    ) : (<div className="row" style={{ paddingTop: '50px' }}>
+        <div className="col-3">
+            <div className="circulo2">
+                <img id="svg" className="svg" src={props.icon} />
+            </div>
+        </div>
+        <div className="col-9">
+            <h4 className="title">{props.title}</h4>
+            <h6 className="subtitle">{props.subtitle}</h6>
+        </div>
+    </div>)
+}
+
+export default ItemSolution
--- src/app/views/home.js
+++ src/app/views/home.js
@@ -2,6 +2,14 @@ import React from 'react'
 import '../css/home.css'
 import Botao from '../components/botao'
 import imgPerfil from '../images/perfil.png'
+import ItemSolution from '../components/itemSolution'
+import pincel from '../images/icons/pincel.svg'
+import engrenagem from '../images/icons/engrenagem.svg'
+import nuvem from '../images/icons/nuvem.svg'
+import escudo from '../images/icons/escudo.svg'
+import mao from '../images/icons/mao.svg'
+import falante from '../images/icons/falante.svg'
+
 
 class Home extends React.Component {
     render() {
@@ -9,13 +17,16 @@ class Home extends React.Component {
             <div className="container">
                 <div className="pageHeader">
                     <div className="title">
+                        {/*Titulo no cabeçalho da pagina*/}
                         <h2>Smart Tool Manage Your Business</h2>
                     </div>
                     <div className="content">
+                        {/*Subtitulo do cabeçalho da pagina*/}
                         <h6>Sydney brings plenty of customization possibilities like access to all Google
                         Fonts, full color control, layout control,logo upload, full screen slider,
                             header image, sticky navigation and much more.</h6>
 
+                        {/*Botoes no topo da pagina*/}
                         <div className="buttonsHeader">
                             <div className="row">
                                 <div className="col-md-6 d-flex justify-content-end">
@@ -27,12 +38,47 @@ class Home extends React.Component {
                             </div>
                         </div>
 
+                        {/*Imagem de perfil*/}
                         <div className="imgPerfil">
                             <img src={imgPerfil} width="100%" />
                         </div>
+
+                        {/*Titulo solutions*/}
+                        <h3>Our Solution For You</h3>
+                        <h6>Consectetur adipiscing elit ut elit tellus, luctus nec ullamcorper mattis.</h6>
+
+                        {/*Primeira fileira solutions*/}
+                        <div className="row">
+                            <div className="col-md-4">
+                                {/*Component solution*/}
+                                <ItemSolution type={1} icon={pincel} title="Professional tools" subtitle="Ipsum pendisse ultrices gravida accumsan lacus." />
+                            </div>
+                            <div className="col-md-4">
+                                <ItemSolution type={2} icon={engrenagem} title="Fully customizable" subtitle="Ipsum pendisse ultrices eyso gravida accumsan lacus." />
+                            </div>
+                            <div className="col-md-4">
+                                <ItemSolution type={1} icon={nuvem} title="Saved to the cloud" subtitle="Quis ipsum pendisse ultrices gravida accumsan lacus." />
+                            </div>
+                        </div>
+
+                        {/*Segunda fileira solutions*/}
+                        <div className="row">
+                            <div className="col-md-4">
+                                <ItemSolution type={2} icon={escudo} title="App integration" subtitle="Ipsum pendisse ultrices eyso gravida accumsan lacus." />
+                            </div>
+                            <div className="col-md-4">
+                                <ItemSolution type={1} icon={mao} title="Eassy to use" subtitle="Ipsum pendisse ultrices eyso gravida accumsan lacus." />
+                            </div>
+                            <div className="col-md-4">
+                                <ItemSolution type={2} icon={falante} title="Maketing" subtitle="Ipsum pendisse ultrices eyso gravida accumsan lacus." />
+                            </div>
+                        </div>
+
+
+
                     </div>
                 </div>
-            </div>
+            </div >
         )
     }
 }
