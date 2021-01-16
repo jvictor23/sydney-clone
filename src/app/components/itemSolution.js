d8bd85ce73e337dfa14e683963692b94c03b123f
João Victor da Silva <joaovictor00788123@gmail.com>
2021-01-16 12:01:08 -0300

    Deixando o component Card e component ItemSolution responsivos

--- src/app/components/itemSolution.js
+++ src/app/components/itemSolution.js
@@ -5,24 +5,24 @@ const ItemSolution = (props) => {
     return props.type === 1 ? (
 
         <div className="row">
-            <div className="col-3 ">
+            <div className="col-lg-3 item">
                 <div className="circulo1">
                     <img id="svg" className="svg" src={props.icon} />
                 </div>
             </div>
-            <div className="col-9">
+            <div className="col-lg-9 item">
                 <h4 className="title">{props.title}</h4>
                 <h6 className="subtitle">{props.subtitle}</h6>
             </div>
         </div>
 
     ) : (<div className="row" >
-        <div className="col-3">
+        <div className="col-lg-3 item">
             <div className="circulo2">
                 <img id="svg" className="svg" src={props.icon} />
             </div>
         </div>
-        <div className="col-9">
+        <div className="col-lg-9 item">
             <h4 className="title">{props.title}</h4>
             <h6 className="subtitle">{props.subtitle}</h6>
         </div>
